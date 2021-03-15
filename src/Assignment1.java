6dfbb6843844b27fca420795d32a08e1b54433d6
Kamal040102 <kamal5201ks@gmail.com>
2021-03-15 13:34:51 +0530

    Assignment-1-Question-3-GUVI

---
+++ src/Assignment1.java
@@ -0,0 +1,13 @@
+public class Assignment1 {
+    public static void main(String[] args) {
+        String Sname = "Sridevi";
+        String city = "Chennai";
+        int age = 32;
+        double height = 5.3;
+        Boolean married = true;
+        char sex = 'F';
+        int population = 4828853;
+
+        System.out.println("Name: " +Sname +"\nCity: " +city+"\nAge: "+age+"\nHeight: "+height+"\nMarried: "+married+"\nSex: "+sex+"\nCity Population: "+population);
+    }
+}
