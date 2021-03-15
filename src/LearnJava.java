79fbf5435854f87f55338adbc7cee8b10ffd28fa
Kamal040102 <kamal5201ks@gmail.com>
2021-03-15 13:39:38 +0530

    Commit

--- src/LearnJava.java
+++ src/LearnJava.java
@@ -1,5 +1,5 @@
-public class LearnJava {
+public class LearnJava{
     public static void main(String[] args) {
-        System.out.println("My name is Kamal.\nThis is my first Java Program.\nAnd I am making the third commit into github now.");       
-    }    
+        System.out.println("Kamal Sharma");
+    }
 }
