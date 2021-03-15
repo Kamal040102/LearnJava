7604bb7b358ced8479fb1a7ec3523ae11e9c7df6
Kamal040102 <kamal5201ks@gmail.com>
2021-03-15 14:42:58 +0530

    Commit

--- src/LearnJava.java
+++ src/LearnJava.java
@@ -1,5 +1,9 @@
 public class LearnJava{
     public static void main(String[] args) {
-        System.out.println("Kamal Sharma");
+        int a = 10;
+        int b = a++;
+        int c = a; 
+
+        System.out.println(b + " " + c + " " + a);
     }
 }
