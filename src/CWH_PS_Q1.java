df3030c5830e4404aa57000b3ec86534a517699b
Kamal040102 <kamal5201ks@gmail.com>
2021-03-21 18:19:13 +0530

    Practice Sets

---
+++ src/CWH_PS_Q1.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class CWH_PS_Q1 {
+    public static void main(String[] args) {
+        Scanner inp = new Scanner (System.in);
+
+        System.out.println("Enter a number");
+        float a = inp.nextFloat();
+        System.out.println("Enter another number");
+        float b = inp.nextFloat();
+        System.out.println("Enter another number");
+        float c = inp.nextFloat();
+        float sum = a + b + c;
+
+        System.out.println("The of those number is\t" + sum);
+    }
+}
