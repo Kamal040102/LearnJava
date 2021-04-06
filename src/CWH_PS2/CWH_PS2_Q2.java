cbb201a80692a8bac9d2788aec7d8b1fd1b382f1
Kamal040102 <kamal5201ks@gmail.com>
2021-04-06 15:11:07 +0530

    PS7

--- src/CWH_PS2/CWH_PS2_Q2.java
+++ src/CWH_PS2/CWH_PS2_Q2.java
@@ -5,8 +5,16 @@ import java.util.Scanner;
 public class CWH_PS2_Q2 {
     public static void main(String[] args) {
         Scanner inp = new Scanner(System.in);
-        System.out.println("Enter your Decrypted Grade:");
-        int grade = inp.nextInt() - 8;
-        System.out.println("Your grade is:\t" + grade);
+
+        // Don't pay attention on Try and finally for now.
+
+        try{                                                     
+            System.out.println("Enter your Decrypted Grade:");
+            int grade = inp.nextInt() - 8;
+            System.out.println("Your grade is:\t" + grade);
+        }
+        finally{
+            inp.close();                   
+        }
     }
 }
--- src/CWH_PS7/CWH_PS7_Q10.java
+++ src/CWH_PS7/CWH_PS7_Q10.java
@@ -1,8 +1,25 @@
 package CWH_PS7;
 
+import java.util.Scanner;
+
 public class CWH_PS7_Q10 {
-    
+    int sum(int x){
+        int sumResult = 0;
+        for(int i = 0; i <= x ; i++){
+            sumResult += i;
+        }
+        return sumResult;
+    }
     public static void main(String[] args) {
-        
+        Scanner inp = new Scanner(System.in);
+        CWH_PS7_Q10 obj3 = new CWH_PS7_Q10();
+        try{
+            System.out.println("Enter a number");
+            int n = inp.nextInt();
+            System.out.println(obj3.sum(n));
+        }
+        finally{
+            inp.close();
+        }
     }
 }
