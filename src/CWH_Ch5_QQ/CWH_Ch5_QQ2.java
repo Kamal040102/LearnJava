e148bdd6a984aff87d3aea464a05cc80e8ae75ca
Kamal040102 <kamal5201ks@gmail.com>
2021-04-06 15:15:32 +0530

    CWH_Ch5_QQ

--- src/CWH_Ch5_QQ/CWH_Ch5_QQ2.java
+++ src/CWH_Ch5_QQ/CWH_Ch5_QQ2.java
@@ -5,13 +5,17 @@ import java.util.Scanner;
 public class CWH_Ch5_QQ2 {
     public static void main(String[] args) {
         Scanner inp = new Scanner(System.in);
-        System.out.println("Enter a number till which you want this program to run.");
-        int i = inp.nextInt();
-        int x = 1;
-        do{
-            System.out.println(x);
-            x++;
+        try {
+            System.out.println("Enter a number till which you want this program to run.");
+            int i = inp.nextInt();
+            int x = 1;
+            do {
+                System.out.println(x);
+                x++;
+            } while (x <= i);
+        } finally {
+            inp.close();
         }
-        while(x <= i);
+
     }
 }
--- src/CWH_Ch5_QQ/CWH_Ch5_QQ3.java
+++ src/CWH_Ch5_QQ/CWH_Ch5_QQ3.java
@@ -6,18 +6,22 @@ public class CWH_Ch5_QQ3 {
     public static void main(String[] args) {
         Scanner inp = new Scanner(System.in);
 
-        System.out.println("Enter a number till which you want to run this program");
-        int i = inp.nextInt();
+        try {
+            System.out.println("Enter a number till which you want to run this program");
+            int i = inp.nextInt();
 
-        int counter = 0;
+            int counter = 0;
 
-        for(int x = 0; x <= i; x++){
-            if(x % 2 != 0){
-                System.out.println(x);
-                counter++;
+            for (int x = 0; x <= i; x++) {
+                if (x % 2 != 0) {
+                    System.out.println(x);
+                    counter++;
+                }
             }
-        }
 
-        System.out.println("Total Odd Numbers Are:\t" + counter);
+            System.out.println("Total Odd Numbers Are:\t" + counter);
+        } finally {
+            inp.close();
+        }
     }
 }
